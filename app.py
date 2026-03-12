51626a97faca324392c3f7719c3e36777eda2564
manucitavm-web <manucitavm@gmail.com>
2026-03-12 13:30:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import streamlit as st
 from PIL import Image
 from googletrans import Translator
 
+import streamlit_lottie import st_lottie
+
 st.title('Análisis de Sentimiento')
 image = Image.open('emoticones.jpg')
 st.image(image)
@@ -35,7 +37,16 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
+            white open("Running Cat.json") as source:
+                      animation=json.load(source)
+            st.lottie(animation,width =350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
+                      white open("Ohhhh CAT.json") as source:
+                      animation=json.load(source)
+            st.lottie(animation,width =350)
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+                      white open("Meditating Giraffe.json.json") as source:
+                      animation=json.load(source)
+            st.lottie(animation,width =350)
